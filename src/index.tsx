8cc0d1ff861baee51ae16513e248ee3135092714
Julio <julio.olvr@gmail.com>
2023-09-22 14:40:21 +0200

    Show Kanji

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,44 @@
 import { useState } from "react";
 import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
 import * as wanakana from "wanakana";
 
 export default function Command() {
   const [text, setText] = useState("");
+  const [selected, setSelected] = useState<string | undefined>(undefined);
   const kana = wanakana.toKana(text);
   const hiragana = wanakana.toHiragana(text);
   const katakana = wanakana.toKatakana(text);
 
+  // TODO: Debounce
+  const query = useFetch<{ main_kanji: string[] }>(`https://kanjiapi.dev/v1/reading/${hiragana}`, {
+    execute: hiragana.trim().length > 0 && wanakana.isKana(hiragana),
+  });
+
   return (
-    <List searchText={text} onSearchTextChange={setText}>
+    <List
+      searchText={text}
+      onSearchTextChange={setText}
+      isLoading={query.isLoading}
+      selectedItemId={selected}
+      isShowingDetail={selected?.startsWith("kanji:")}
+      onSelectionChange={(newId) => (newId === null ? setSelected(undefined) : setSelected(newId))}
+    >
       {text.trim().length > 0 && (
         <>
           <List.Item icon="list-icon.png" title={`Kana: ${kana}`} actions={<Actions text={kana} />} />
           <List.Item icon="list-icon.png" title={`Hiragana: ${hiragana}`} actions={<Actions text={hiragana} />} />
           <List.Item icon="list-icon.png" title={`Katakana: ${katakana}`} actions={<Actions text={katakana} />} />
+          {query.data?.main_kanji.map((kanji) => (
+            <List.Item
+              key={kanji}
+              icon="list-icon.png"
+              title={`Kanji: ${kanji}`}
+              actions={<Actions text={kanji} />}
+              detail={<KanjiDetail kanji={kanji} />}
+              id={`kanji:${kanji}`}
+            />
+          ))}
         </>
       )}
     </List>
@@ -26,5 +50,43 @@ function Actions({ text }: { text: string }) {
     <ActionPanel>
       <Action.CopyToClipboard title="Copy to Clipboard" content={text} />
     </ActionPanel>
+  );
+}
+
+function KanjiDetail({ kanji }: { kanji: string }) {
+  const query = useFetch<{
+    kun_readings: string[];
+    on_readings: string[];
+    meanings: string[];
+    grade: number | null;
+    heisig_en: string | null;
+  }>(`https://kanjiapi.dev/v1/kanji/${kanji}`);
+
+  return (
+    <List.Item.Detail
+      isLoading={query.isLoading}
+      metadata={
+        <List.Item.Detail.Metadata>
+          <List.Item.Detail.Metadata.Label title="Kanji" text={kanji} />
+          <List.Item.Detail.Metadata.Label title="Keyword" text={query.data?.heisig_en || ""} />
+          <List.Item.Detail.Metadata.Label title="Grade" text={query.data?.grade?.toString(10) || "?"} />
+          <List.Item.Detail.Metadata.Separator />
+          <List.Item.Detail.Metadata.Label title="Meanings" />
+          {query.data?.meanings.map((meaning) => (
+            <List.Item.Detail.Metadata.Label key={meaning} title={meaning} />
+          ))}
+          <List.Item.Detail.Metadata.Separator />
+          <List.Item.Detail.Metadata.Label title="Kun-yomi readings" />
+          {query.data?.kun_readings.map((reading) => (
+            <List.Item.Detail.Metadata.Label key={reading} title={reading} />
+          ))}
+          <List.Item.Detail.Metadata.Separator />
+          <List.Item.Detail.Metadata.Label title="On-yomi readings" />
+          {query.data?.on_readings.map((reading) => (
+            <List.Item.Detail.Metadata.Label key={reading} title={reading} />
+          ))}
+        </List.Item.Detail.Metadata>
+      }
+    />
   );
 }
